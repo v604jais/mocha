e2038cb98a9a1c6d73971b126ebdc796deeb55fa
v04jais <vjaiswal604@gmail.com>
2020-04-12 22:52:15 +0530

    added all the file

---
+++ test/mo_helper_test.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+mongoose.promise = global.promise; //ES6 -promise
+
+//hooks
+before((done) => {
+
+    mongoose.connect("mongodb://localhost/mongotube", { useNewUrlParser: true });
+
+    mongoose.connection
+        .once("open", () => {
+            // console.log("Connected");
+            done();
+        })
+        .on("error", error => {
+            console.log("Your Error", error);
+        });
+});
+
